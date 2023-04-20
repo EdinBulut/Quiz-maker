813a47934386eb265a33148b90ebdd40772ea698
EdoB <edin.bulut@hotmail.com>
2023-04-21 01:19:55 +0200

    testing BE in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuestionsService } from './services/questions.service';
+import { Observable, tap } from 'rxjs';
+import { Question } from './models/question-model';
+import { } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Quiz-maker';
+  questions$!: Observable<Question[]>
+  constructor(private questionsService: QuestionsService) { }
+
+
+  ngOnInit(): void {
+    this.questions$ = this.questionsService.getQuestions()
+    .pipe(
+      tap(data => console.log(data))
+    )
+  }
+
 }
