2fbff1aef1b83c56b7792953ed960cc17919a41f
EdoB <edin.bulut@hotmail.com>
2023-04-21 01:18:00 +0200

    question schema created

---
+++ server/models/question.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const questionSchema = new Schema({
+  question: String,
+  answer: String
+}, { versionKey: false })
+
+module.exports = mongoose.model('question', questionSchema, 'questions')
