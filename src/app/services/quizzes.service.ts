4266d44c6670318ce48a6cabfc25a9461b164645
EdoB <edin.bulut@hotmail.com>
2023-04-21 15:42:59 +0200

    create quiz added to quizzes service

--- src/app/services/quizzes.service.ts
+++ src/app/services/quizzes.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Quiz } from '../models/quiz-model';
 
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -14,18 +16,23 @@ export class QuizzesService {
 
   constructor(private http: HttpClient) { }
 
-  getQuizzes(): Observable<Quiz> {
-    return this.http.get<Quiz>(this.quizzesUrl);
+  getQuizzes(): Observable<Quiz[]> {
+    return this.http.get<Quiz[]>(this.quizzesUrl);
+  }
+  
+
+  createQuiz(quiz: {name: string, questionIDs: string[]}): Observable<any> {
+    return this.http.post<any>(this.quizzesUrl, quiz, this.httpOptions);
   }
 
 
-  getQuiz(search: string): Observable<Quiz> {
-    const url = `${this.quizzesUrl}/${search}`;
+  getQuiz(id: string): Observable<Quiz> {
+    const url = `${this.quizzesUrl}/${id}`;
     return this.http.get<Quiz>(url);
   }
   
   searchQuizzes(search: string): Observable<Quiz> {
-    const url = `${this.quizzesUrl}/${search}`;
+    const url = `${this.quizzesUrl}/search/${search}`;
     return this.http.get<Quiz>(url);
   }
 
