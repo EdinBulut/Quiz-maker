693c528be7896eb78c7a5742b791cae40a5a39cd
EdoB <edin.bulut@hotmail.com>
2023-04-21 04:59:54 +0200

    testing BE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { QuestionsService } from './services/questions.service';
 import { Observable, tap } from 'rxjs';
 import { Question } from './models/question-model';
 import { } from 'rxjs/operators'
+import { QuizzesService } from './services/quizzes.service';
 
 @Component({
   selector: 'app-root',
@@ -12,14 +13,19 @@ import { } from 'rxjs/operators'
 export class AppComponent implements OnInit{
   title = 'Quiz-maker';
   questions$!: Observable<Question[]>
-  constructor(private questionsService: QuestionsService) { }
+  constructor(
+    private questionsService: QuestionsService,
+    private quizzesService: QuizzesService
+    ) { }
 
 
   ngOnInit(): void {
     this.questions$ = this.questionsService.getQuestions()
     .pipe(
-      tap(data => console.log(data))
+      tap(data => console.log('questions', data))
     )
+    this.quizzesService.getQuizzes().subscribe(data => console.log('all quizzes', data))
+    this.quizzesService.searchQuizzes('TEST').subscribe(data => console.log('searched quizzes', data))
   }
 
 }
