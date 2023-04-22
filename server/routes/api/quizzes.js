e7fe00bef6d4d46d7f6645ce94983c4c4a247cc4
EdoB <edin.bulut@hotmail.com>
2023-04-23 01:35:59 +0200

    unecessary import removed

--- server/routes/api/quizzes.js
+++ server/routes/api/quizzes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const Question = require('../../models/question');
 const Quiz = require('../../models/quiz');
 const mongoose = require('mongoose');
 
