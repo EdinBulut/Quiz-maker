eb6eed4894da35c5a9b8a56e04e43bff52f540d2
EdoB <edin.bulut@hotmail.com>
2023-04-23 19:03:49 +0200

    routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/quizzes', pathMatch: 'full'},
   {
     path: 'quizzes',
     loadChildren: () => import('./quizzes/quizzes.module').then(m => m.QuizzesModule)
