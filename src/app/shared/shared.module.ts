377b470598f450ea10d818429321b642c2bf91c0
EdoB <edin.bulut@hotmail.com>
2023-04-23 22:42:10 +0200

    shared module created

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
+
+@NgModule({
+  declarations: [
+  ],
+  exports: [
+    MatProgressBarModule
+  ]
+})
+
+export class SharedModule { }
