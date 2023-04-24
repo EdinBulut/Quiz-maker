28df3eb407a4bff705107e0b0904ce3608ec51e1
EdoB <edin.bulut@hotmail.com>
2023-04-24 02:22:46 +0200

    option for delete quiz added

--- src/app/quizzes/quizzes.component.ts
+++ src/app/quizzes/quizzes.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, debounceTime, delay, tap } from 'rxjs';
 import { Quiz } from './models/quiz-model';
 import { QuizzesService } from './services/quizzes.service';
 import { skeletonLoaderData } from '../shared/constants/skeleton.constants';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../shared/components/dialogs/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-quizzes',
@@ -16,7 +18,8 @@ export class QuizzesComponent implements OnInit {
   isSearchInFocus = false
 
   constructor(
-    private quizzesService: QuizzesService
+    private quizzesService: QuizzesService,
+    private dialog: MatDialog,
   ) { }
 
   ngOnInit(): void {
@@ -25,9 +28,50 @@ export class QuizzesComponent implements OnInit {
 
   getQuizzes() {
     this.quizzes$ = this.quizzesService.getQuizzes()
-    .pipe(
-      tap(quizes => console.log(quizes))
-    )
+      .pipe(
+        delay(1000),
+        tap(quizes => console.log(quizes))
+      )
   }
+
+
+  deleteQuiz(quizForRemove: Quiz) {
+    console.log('remove me')
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      width: '100%',
+      maxWidth: '456px',
+      minHeight: '276px',
+      panelClass: 'bottom-to-top',
+      data: this.confirmDeleteQuizData(quizForRemove)
+    })
+
+    dialogRef.afterClosed().subscribe(confirmed => {
+      if (confirmed) {
+        this.isProcessing = true
+      }
+    })
+  }
+
+
+  confirmDeleteQuizData(quizForRemove: Quiz) {
+    const messageTxt = `Are you sure you want to delete quiz <strong>${quizForRemove.name}</strong>?`
+    const dialogVariables =  {
+        title: 'Delete quiz',
+        message: {
+          text: messageTxt,
+          isCentered: true
+        },
+        cancelBtn: {
+          text: 'No'
+        },
+        confirmBtn: {
+          text: 'Yes'
+        },
+    }
+    console.log(dialogVariables)
+    return dialogVariables
+  }
+
+
 
 }
