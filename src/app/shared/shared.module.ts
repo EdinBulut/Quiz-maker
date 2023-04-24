21eba0e88231cc954bf3ea61f642d617b415b368
EdoB <edin.bulut@hotmail.com>
2023-04-24 02:24:02 +0200

    shared model - refactored code

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-import {MatRippleModule} from '@angular/material/core';
+import { MatRippleModule } from '@angular/material/core';
+import { MatCardModule } from '@angular/material/card';
+import { ConfirmDialogComponent } from './components/dialogs/confirm-dialog/confirm-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
 
+const materialModules = [
+  MatProgressBarModule,
+  MatRippleModule,
+  MatCardModule,
+  MatDialogModule,
+]
 
 @NgModule({
   declarations: [
+    ConfirmDialogComponent,
   ],
+  imports: [
+    CommonModule,
+    materialModules
+  ],
+
   exports: [
-    MatProgressBarModule,
-    MatRippleModule
+    CommonModule,
+    materialModules,
+    ConfirmDialogComponent
   ]
 })
 
