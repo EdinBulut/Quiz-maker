90717ca3589062a6474ac27b78554a76f70f1e04
EdoB <edin.bulut@hotmail.com>
2023-04-24 05:28:47 +0200

    delete quiz connected with BE

--- src/app/quizzes/quizzes.component.ts
+++ src/app/quizzes/quizzes.component.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, debounceTime, delay, tap } from 'rxjs';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Observable, delay, filter, from, map, shareReplay, take, tap, toArray } from 'rxjs';
 import { Quiz } from './models/quiz-model';
 import { QuizzesService } from './services/quizzes.service';
 import { skeletonLoaderData } from '../shared/constants/skeleton.constants';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmDialogComponent } from '../shared/components/dialogs/confirm-dialog/confirm-dialog.component';
+import { DialogVariables } from '../shared/models/dialog-variables.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { NoopScrollStrategy } from '@angular/cdk/overlay';
+
 
 @Component({
   selector: 'app-quizzes',
@@ -20,55 +24,84 @@ export class QuizzesComponent implements OnInit {
   constructor(
     private quizzesService: QuizzesService,
     private dialog: MatDialog,
+    private cdRef: ChangeDetectorRef,
+    private snackBar: MatSnackBar
   ) { }
+
+
 
   ngOnInit(): void {
     this.getQuizzes()
   }
 
+
+
   getQuizzes() {
     this.quizzes$ = this.quizzesService.getQuizzes()
       .pipe(
-        delay(1000),
-        tap(quizes => console.log(quizes))
+        delay(500),
+        tap(quizes => console.log(quizes)),
+        shareReplay(1)
       )
   }
 
 
-  deleteQuiz(quizForRemove: Quiz) {
-    console.log('remove me')
+
+  deleteQuiz(quiz: Quiz) {
     const dialogRef = this.dialog.open(ConfirmDialogComponent, {
       width: '100%',
       maxWidth: '456px',
       minHeight: '276px',
       panelClass: 'bottom-to-top',
-      data: this.confirmDeleteQuizData(quizForRemove)
+      data: this.confirmDeleteQuizData(quiz.name),
+      scrollStrategy: new NoopScrollStrategy()
     })
 
     dialogRef.afterClosed().subscribe(confirmed => {
-      if (confirmed) {
-        this.isProcessing = true
-      }
+      if (confirmed) this.onDeleteQuizConfirmed(quiz._id)
     })
   }
 
 
-  confirmDeleteQuizData(quizForRemove: Quiz) {
-    const messageTxt = `Are you sure you want to delete quiz <strong>${quizForRemove.name}</strong>?`
-    const dialogVariables =  {
-        title: 'Delete quiz',
-        message: {
-          text: messageTxt,
-          isCentered: true
-        },
-        cancelBtn: {
-          text: 'No'
-        },
-        confirmBtn: {
-          text: 'Yes'
-        },
+
+  onDeleteQuizConfirmed(quizID: string) {
+    this.isProcessing = true
+    this.quizzesService.deleteQuiz(quizID).subscribe(
+      quiz => {
+        this.quizzes$ = this.quizzes$.pipe(
+          map(quizzes => quizzes.filter(q => q._id !== quiz._id)),
+          tap(() => {
+            this.isProcessing = false
+            this.cdRef.detectChanges()
+            const message = 'Quiz successfully deleted'
+            this.snackBar.open(message, '', {
+              horizontalPosition: 'center',
+              duration: 3000
+            });
+          })
+        )
+      },
+      err => console.log(err)
+      )
+  }
+  
+
+
+  confirmDeleteQuizData(quizName: string) {
+    const messageTxt = `Are you sure you want to delete quiz <strong>${quizName}</strong>?`
+    const dialogVariables: DialogVariables = {
+      title: 'Delete quiz',
+      message: {
+        text: messageTxt,
+        isCentered: true
+      },
+      cancelBtn: {
+        text: 'No'
+      },
+      confirmBtn: {
+        text: 'Yes'
+      },
     }
-    console.log(dialogVariables)
     return dialogVariables
   }
 
